73ab08f1e9b9f1604313047f97343c6dbd1eb997
dora-ljh <lijihui_WYY@163.com>
2023-04-06 21:54:23 +0800

    fix: 全排列 II

--- srcipts/permuteUnique/index.js
+++ srcipts/permuteUnique/index.js
@@ -2,8 +2,60 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+// export var permuteUnique = function(nums) {
+//   // 排序的用处是会把重复元素放在一起
+//   nums.sort((a,b)=>a - b);
+//   const res = [];
+//   const track = [];
+//   const used = new Array(nums.length).fill(false);
+//   const backtrack = function (){
+//     if(track.length === nums.length){
+//       res.push(track.slice());
+//       return;
+//     }
+//     for(let i = 0; i < nums.length; i++){
+//       if(used[i])continue;
+//       /**
+//         保证相同元素在排列中的相对位置保持不变
+//         i > 0 代表 我只有有前一个元素，才能取到 nums[i - 1]
+//         nums[i] === nums[i - 1] 如果上一个元素跟我是一样的
+//         !used[i - 1] 是说上一个元素没有用到
+//         如果同层级的上一个元素没有用到，则直接跳过
+//         就是说要保证 2 一定要在2'前边，比如如果不写这一个判断
+//        [
+//        [ 1, 2, 2' ],[ 1, 2', 2 ],
+//        [ 2, 1, 2' ],[ 2, 2', 1 ],
+//        [ 2', 1, 2 ],[ 2', 2, 1 ]
+//        ]
+//        其实只要把2'在2前边的给过滤掉，就可以了，就会剩下
+//        [ [ 1, 2, 2' ], [ 2, 1, 2' ], [ 2, 2', 1 ] ]
+//
+//        标准全排列算法之所以出现重复，是因为把相同元素形成的全排列视为不同的序列，
+//        但实际上他们应该是相同的，
+//        而如果固定相同元素形成的序列顺序，当然就避免了重复。
+//
+//        当出现重复元素时，比如输入 nums = [1,2,2',2'']，2'只有在2已经被使用的情况下才会被选择
+//        同理，
+//        2''只有在2'已经被使用的情况下才会被选择，这就保证了相同元素在排列中的相对位置保证固定
+//
+//       * */
+//
+//       if(i > 0 && nums[i] === nums[i - 1] && !used[i - 1])continue;
+//       used[i] = true;
+//       track.push(nums[i]);
+//       backtrack(i);
+//       used[i] = false;
+//       track.pop();
+//     }
+//   }
+//   backtrack();
+//   return res;
+// };
+
+
+
 export var permuteUnique = function(nums) {
-  // 排序的用处是会把重复元素放在一起
+  // 排序不能少，因为下边还是跟前一项进行对比
   nums.sort((a,b)=>a - b);
   const res = [];
   const track = [];
@@ -13,36 +65,18 @@ export var permuteUnique = function(nums) {
       res.push(track.slice());
       return;
     }
+    // 这里去记录一下前一条树枝的值
+    let preNum = -666;
     for(let i = 0; i < nums.length; i++){
       if(used[i])continue;
-      /**
-        保证相同元素在排列中的相对位置保持不变
-        i > 0 代表 我只有有前一个元素，才能取到 nums[i - 1]
-        nums[i] === nums[i - 1] 如果上一个元素跟我是一样的
-        !used[i - 1] 是说上一个元素没有用到
-        如果同层级的上一个元素没有用到，则直接跳过
-        就是说要保证 2 一定要在2'前边，比如如果不写这一个判断
-       [
-       [ 1, 2, 2' ],[ 1, 2', 2 ],
-       [ 2, 1, 2' ],[ 2, 2', 1 ],
-       [ 2', 1, 2 ],[ 2', 2, 1 ]
-       ]
-       其实只要把2'在2前边的给过滤掉，就可以了，就会剩下
-       [ [ 1, 2, 2' ], [ 2, 1, 2' ], [ 2, 2', 1 ] ]
+      // 这里也不用进行判断
+      // if(i > 0 && nums[i] === nums[i - 1] && !used[i - 1])continue;
 
-       标准全排列算法之所以出现重复，是因为把相同元素形成的全排列视为不同的序列，
-       但实际上他们应该是相同的，
-       而如果固定相同元素形成的序列顺序，当然就避免了重复。
-
-       当出现重复元素时，比如输入 nums = [1,2,2',2'']，2'只有在2已经被使用的情况下才会被选择
-       同理，
-       2''只有在2'已经被使用的情况下才会被选择，这就保证了相同元素在排列中的相对位置保证固定
-
-      * */
-
-      if(i > 0 && nums[i] === nums[i - 1] && !used[i - 1])continue;
+      // 这里判断下当前值和前一项是否相等，如果相等，则不处理
+      if(nums[i] === preNum) continue;
       used[i] = true;
       track.push(nums[i]);
+      preNum = nums[i];
       backtrack(i);
       used[i] = false;
       track.pop();
@@ -52,4 +86,4 @@ export var permuteUnique = function(nums) {
   return res;
 };
 
-// console.log(permuteUnique([1,2,2]));
+// console.log(permuteUnique([2,1,2]));
